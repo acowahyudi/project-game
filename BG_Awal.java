8e6942983c2078d18869c99d56ca69da3c1ccf08
acowahyudi <acoxs0707@gmail.com>
2018-05-13 23:40:08 +0800

    Push Ke8 (set button keluar di awal game)

--- BG_Awal.java
+++ BG_Awal.java
@@ -18,9 +18,9 @@ public class BG_Awal extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 400, 1);
         setPaintOrder(Mulai.class);
-        
+        setPaintOrder(Keluar.class);
         addObject(new Mulai(),580,286);     
-        
+        addObject(new Keluar(),580,343);
         
     }
 }
