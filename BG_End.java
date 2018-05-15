88172f4727573cb24173cc87fcc09f05f4311e41
DESKTOP-561UL9D\Sandra <ms052946@gmail.com>
2018-05-15 11:13:41 +0700

    push ke8 (update sound gameover)

--- BG_End.java
+++ BG_End.java
@@ -17,6 +17,7 @@ public class BG_End extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 400, 1); 
+        Greenfoot.playSound("Gameover.mid");
         prepare();
     }
 
